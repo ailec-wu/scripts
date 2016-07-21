730d09ca4c6812aa7ffa256afccb5dfbf828155c
sam <sambhavs.email@gmail.com>
2016-07-21 20:16:23 +0530

    Added DR Calculator

--- drcalc.py
+++ drcalc.py
@@ -15,15 +15,27 @@ for i in pdfs:
 	with open(i) as f:
 		x = slate.PDF(f)
 		try:
+			txt = x[0].split()
 			cpi=x[0].split()[-8]
 			
 			roll=x[0].split()[19][1:]
 			if len(cpi)!=4:
-				print i	
-			final.append([cpi,roll])
+				print i
+			# print x[0].split()
+			ns= 0
+			ne = 0
+			for j in range(len(txt)):
+				if "NAME" in txt[j]:
+					ns=j+1
+				if "DISCIPLINE" in txt[j]:
+					ne= j
+			txt[ns] = txt[ns][1:]
+			name = " ".join(txt[ns:ne])
+			final.append([cpi,roll,name])		
+
 		except:
 			print i	
 final.sort(reverse=True)
-f = open("sorted.csv","w")
+f = open("sorted1.csv","w")
 for i in final:
-	f.write(i[0]+","+i[1]+"\n")
+	f.write(i[0]+","+i[1]+","+i[2]+"\n")
